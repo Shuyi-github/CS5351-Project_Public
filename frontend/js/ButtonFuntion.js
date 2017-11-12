a85231a158a84e5d6a71b768834adc8f2629c407
Tang9511 <shuyitang4-c@my.cityu.edu.hk>
2017-11-12 22:10:51 +0800

    0.2.1
    modification

--- frontend/js/ButtonFuntion.js
+++ frontend/js/ButtonFuntion.js
@@ -10,28 +10,50 @@ function hidediv(){
 
 
 function insertclient() {
-
-      $.ajax({
-          type: "POST",     //提交方式
-          //contentType: "application/json; charset=utf-8",   //内容类型
-          dataType: "json",     //传回类型
-          url: 'backend/test.php',
-          data: {
-              client_name: $("input[name='client_name']").val(),
-              client_address:$("input[name='client_address']").val(),
-              contact_information:$("input[name='contact_information']").val(),
-          },
-          success: function (data) {
-           //   alert(data.status);        //用data.d来获取后台传过来的json语句，或者是单纯的语句
-              if(!data.status){
-                  alert("save success");
-              }
-              else alert("server error");
-          },
-          error: function (err) {
-              alert("err:" + err);
-          }
-      });
+    var name = $("input[name='client_name']").val();
+    var address = $("input[name='client_address']").val();
+    var information = $("input[name='contact_information']").val();
+    if(name.length < 1 || address.length < 1) {
+        alert("Please enter Client Name and Address.");
+    } else {
+        $.ajax({
+            type: "POST",     //提交方式
+            //contentType: "application/json; charset=utf-8",   //内容类型
+            dataType: "json",     //传回类型
+            url: 'backend/test.php',
+            data: {
+                client_name: name,
+                client_address: address,
+                contact_information: information,
+            },
+            success: function (data) {
+             //   alert(data.status);        //用data.d来获取后台传过来的json语句，或者是单纯的语句
+                if(!data.status){
+                    alert("save success");
+                    hidediv();
+                }
+                else alert("server error");
+            },
+            error: function (err) {
+                alert("err:" + err);
+                hidediv();
+    /* for temporary save on frontend
+                var x=document.createElement("button");
+                x.setAttribute("type","button");
+                x.setAttribute("class","jqbtn");
+                x.setAttribute("id","insertbtn");
+                x.innerHTML =$("input[name='client_name']").val();
+              $("#insert").append("<br>");
+              $("#insert").append(x);
+              $("#insertbtn").click(function(){
+                  $("#fade").show();
+                  $("#light").show();
+              });
+        //      $("#insert").append("<li>"+$("input[name='client_name']").val()+"</li>");
+    */
+           }
+        });
+    }
 }
 
 //jquery
@@ -67,7 +89,4 @@ $(document).ready(function(){
     $("#btn5").click(function(){
         $("#brline").append(" <li>Appended text</li>");
     });
-
 })
-
-
