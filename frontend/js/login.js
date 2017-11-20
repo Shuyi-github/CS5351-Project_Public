e6ffe94142b6fccbfe0c74d426c72effe58f30ba
haiye1992 <31914153+haiye1992@users.noreply.github.com>
2017-11-20 16:34:28 +0800

    0.2.7

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,14 +1,14 @@
 function confirm(){
-    var user=$('#user').val()//获取页面中登录名和密码
+    var user=$('#user').val()                             //获取页面中登录名和密码
     var pwd=$('#password').val();
     var hash_pas = hex_md5(pwd);
-    if(user==""|| hash_pas==""){                          //判断两个均不为空（其他判断规则在其输入时已经判断）
+    if(user=="" || hash_pas==""){                          //判断两个均不为空（其他判断规则在其输入时已经判断）
         alert(" You should enter your name or password!")
         return false;
-    }else{                                       //以上均符合要求，则调用登录esb接口
+    }else{
         $.post('backend/login/login', {
                 username:user,
-                password:hash_pas,        //向服务器（接口）传递的参数
+                password:hash_pas,                           //向服务器（接口）传递的参数
             }, function(data) {                                        //服务器（接口）返回来的数据
                 console.log(data);
                 if(data.status == 0) {/*
