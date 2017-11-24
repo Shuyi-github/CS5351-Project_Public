a992db777b1b838b7aef4aa64ca77f7facff5338
haiye1992 <31914153+haiye1992@users.noreply.github.com>
2017-11-24 17:28:39 +0800

    0.3.0

--- frontend/js/payrate.js
+++ frontend/js/payrate.js
@@ -10,9 +10,9 @@ function topayrate(){
         },
         success: function (data) {
             if (!data.status) {
-                $("#payratestaff").append('<table id="payratetable" style="align-content: center" align="center" width="1200"><tr><th align="center">'+"NAME"+'</th> <td align="center">'+"role"+'</td><td align="center">'+"payrate"+'</td></tr></table>');
+                $("#payratestaff").append('<table id="payratetable" style="align-content: center" align="center" width="800"><tr><th align="center">'+"NAME"+'</th> <td align="center">'+"role"+'</td><td align="center">'+"payrate"+'</td></tr></table>');
                 $.each(data,function (key,value) {
-                    $("#payratestaff").append('<table id="payratetable" style="align-content: center" align="center" width="1200"><tr><td align="center">'+value.name+'</td> <td align="center">'+value.rolename+'</td><td align="center">'+value.staffpayrate+'</td></tr></table>')
+                    $("#payratestaff").append('<table id="payratetable" style="align-content: center" align="center" width="800"><tr><td align="center">'+value.name+'</td> <td align="center">'+value.rolename+'</td><td align="center">'+value.staffpayrate+'</td></tr></table>')
 
                 });
                  $('#selectpayrate').replaceWith('<select class="selectaddpr" id="selectpayrate">');
